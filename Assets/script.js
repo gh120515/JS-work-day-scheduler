0a3b1db4c878871e81fed93a057b870247b115f7
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-04-16 17:39:28 +0930

    added timeblock class feature, not yet functional

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,8 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-
-// Date & time powered by DayJS
-let currentTimeDay = dayjs();
-$("#currentDay").text(currentTimeDay.format(' MMM D, YYYY, h:mm a'));
-
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
 $(document).ready(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -37,10 +17,6 @@ $(document).ready(function () {
     // this - bubbles up from <i> to the siblings/parents (<div>, text)
     let time = $(this).parent().attr("id");
     let description = $(this).siblings(".description").val();
-
-    console.log(time);
-    console.log(description);
-
     // save the item in localStorage
     localStorage.setItem(time, description);
   })
@@ -50,15 +26,41 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
-  
 
+  // get current hour from dayJS
+  let currentHour = dayjs().hour();
+  console.log("Current Hour in DayJS is " + currentHour);
+
+  // use .each jQuery function to execute function for all timeblocks (9-5PM)
+  $(".timeblock").each(function (){
+    // split the hour-id in the timeblocks so it can be compared with DayJS time
+    let timeBlockNow = $(this).attr("id").split("-")[1];
+
+    // compare current Hour from DayJS to match time in the timeblocks
+    // add / remove classes based on current time
+    if (currentHour == timeBlockNow) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+    } else if (currentHour < timeBlockNow) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeBlockNow) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  })
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
+  
+
   // TODO: Add code to display the current date in the header of the page.
+  // Date & time powered by DayJS
+    let currentTimeDay = dayjs();
+    $("#currentDay").text(currentTimeDay.format(' MMM D, YYYY, h:mm a'));
+
 });
 
 
